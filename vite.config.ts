66f471a3438af1f0e67d2ac4f9e784fa189d72b9
VBL97 <riigs97@gmail.com>
2025-03-27 12:36:55 +0300

    tech: замена webpack на vite

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    server: {
+      port: 5000,
+      open: true,
+      hot: true,
+    },
+  };
+});
